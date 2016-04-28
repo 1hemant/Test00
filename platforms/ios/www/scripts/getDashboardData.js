7fd368402c485994b20bf5cb82feba1eeb6f085e
1hemant <hrathore@Netlinks-Mac-mini-3.local>
2016-04-28 09:53:16 +0530

    Waiting for the services
    Waiting for the services to resume.

---
+++ platforms/ios/www/scripts/getDashboardData.js
@@ -0,0 +1,30 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+function getDashboardData(userId,workboard_id) {
+    var dashboardData;
+    var requestData ={"data":{"userId":userId}};
+    $.ajax({
+        url: "http://10.90.21.43:8081/repository-service/getRepository",
+        type: "POST",
+        data: JSON.stringify(requestData),
+        contentType: 'application/json; charset=utf-8',
+        dataType: 'json',
+        
+		//beforeSend: function(xhr){xhr.setRequestHeader('AccessToken',localStorage.getItem("accessToken"));},
+        success: function(object)
+        { 
+           dashboardData=object;     
+        },
+		error: function(error)
+		{
+			alert('Error in getDashboardData;' + eval(error));
+		}
+    });
+  return dashboardData;
+}
+ 
---
+++ platforms/ios/www/scripts/getWorkboardData.js
@@ -0,0 +1,27 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+function getWorkboardData(userId,workboard_id) {
+var requestData = {"data":{"userId":userId,"workboard-id":workboard_id}};
+$.ajax({
+     url: "http://10.90.21.43:8081/workboard-service/get-workboard",               
+     type: "POST",
+     data: JSON.stringify(requestData),
+     contentType: 'application/json; charset=utf-8',
+     dataType: 'json',
+     
+		//beforeSend: function(xhr){xhr.setRequestHeader('AccessToken',localStorage.getItem("accessToken"));},
+     success: function(object)
+     { 
+               workboardData=object;
+				console.log(workboardData);
+     },
+	error: function(error)
+	{
+		alert('Error in getWorkboardData;' + eval(error));
+	}
+  });
+  return workboardData;
+}
