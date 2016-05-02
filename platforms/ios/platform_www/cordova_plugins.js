3ee1c7a55cac440328e536d831f29084dda46424
1hemant <hrathore@Netlinks-Mac-mini-3.local>
2016-05-02 15:57:52 +0530

    Plugin added.
    cordova plugin add cordova-plugin-device
    cordova plugin add cordova-plugin-sqlite

--- platforms/ios/platform_www/cordova_plugins.js
+++ platforms/ios/platform_www/cordova_plugins.js
@@ -1,9 +1,29 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "id": "cordova-plugin-device.device",
+        "pluginId": "cordova-plugin-device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-sqlite/www/SQLitePlugin.js",
+        "id": "cordova-plugin-sqlite.SQLitePlugin",
+        "pluginId": "cordova-plugin-sqlite",
+        "clobbers": [
+            "window.sqlitePlugin",
+            "cordova.plugins.sqlitePlugin"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.2.2"
+    "cordova-plugin-whitelist": "1.2.2",
+    "cordova-plugin-device": "1.1.2",
+    "cordova-plugin-sqlite": "1.0.3"
 }
 // BOTTOM OF METADATA
 });
